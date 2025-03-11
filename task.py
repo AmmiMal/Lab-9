66e2cde2eec43de7e8646ebc03f935ecaafdfecf
AmmiMal <malyshkina.a.m@gmail.com>
2025-03-11 21:14:02 +0300

    little code update

--- task.py
+++ task.py
@@ -1,28 +1,12 @@
 # Портфолио разработчика. Поля ввода данных: названием, ссылки на репозиторий (рабочая). БД: title, link
 # Сделать очистку ленты сообщений, запускаемую кнопкой "Clear".
 
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("Developer's portfolio")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lab.db'
 db = SQLAlchemy(app)
-
-
-products = [
-    {'prod_name': 'sofa',
-     'price': 12000,
-     'in_stock': False,
-     'id': 0},
-    {'prod_name': 'table',
-     'price': 6000,
-     'in_stock': True,
-     'id': 1},
-    {'prod_name': 'chair',
-     'price': 8000,
-     'in_stock': False,
-     'id': 2},
-]
 
 
 class Portfolio(db.Model):
@@ -41,13 +25,6 @@ def index():
     return render_template('lab_9.html', project_list=projects)
 
 
-# @app.route('/notes', methods=['GET'])
-# def get_notes():
-#     notes = Note.query.all()
-#     notes_list = [{'id': note.id, 'text': note.text, 'important': note.important} for note in notes]
-#     return jsonify(notes_list)
-
-
 @app.route('/add', methods=['POST'])
 def add_project():
     data = request.get_json()
@@ -62,7 +39,6 @@ def add_project():
 def clear_notes():
     db.session.query(Portfolio).delete()
     db.session.commit()
-    return jsonify({'message': 'All notes cleared!'})
 
 
 if __name__ == '__main__':
